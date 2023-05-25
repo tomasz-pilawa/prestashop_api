68437ba0bf1ea397c5ec08ebc85fcf669d9ebcf1
Tuan <tomasz.pilawa@gmail.com>
2023-05-25 13:47:08 +0200

    Test getting broducts by brand from a JSON (faster) in main

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
 import api_products as ap
+import json
 
 brands = ap.get_all_manufacturers()
 
 print(brands)
 
-extracted_products_1 = ap.get_products(ap.get_products_indexes(50), brands[3])
+# extracted_products_1 = ap.get_products(ap.get_products_indexes(50), brands[3])
+#
+# for extr in extracted_products_1:
+#     print(extr['name']['language']['value'])
+#     print(extr['manufacturer_name']['value'])
+#     print(extr)
 
-for extr in extracted_products_1:
+brand_test = 'Filorga'
+
+with open('brands_mapped.json') as file:
+    data = json.load(file)
+    product_indexes = data[brand_test]
+
+print(product_indexes)
+
+extracted_products_2 = ap.get_products(product_indexes, brand_test)
+
+for extr in extracted_products_2:
     print(extr['name']['language']['value'])
-    print(extr['manufacturer_name']['value'])
     print(extr)
 
