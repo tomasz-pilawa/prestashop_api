f4dab852dfcad372eb9c18881c6c5bcfc5da51aa
Tuan <tomasz.pilawa@gmail.com>
2023-06-02 11:58:24 +0200

    Add_from_xml imrpoved code clarity and added mode parameter for easier use

--- add_product.py
+++ add_product.py
@@ -53,7 +53,7 @@ def add_product_from_csv(product):
     return data
 
 
-def add_from_xml(file_name, brand, to_print=0, price_ratio=1.87):
+def add_from_xml(file_name, brand, mode='print', price_ratio=1.87, max_products=3):
 
     tree = ET.parse(file_name)
     root = tree.getroot()
@@ -73,51 +73,48 @@ def add_from_xml(file_name, brand, to_print=0, price_ratio=1.87):
         if product_sku in sku_list:
             selected_products.remove(product)
 
-    if to_print == 1:
+    if mode == 'print':
         print(f'\nThere are potentially {len(selected_products)} products to add from the XML file\n')
         for p in selected_products:
             print(p.find('name').text)
             print(p.get('id'))
             print(p.find("attrs/a[@name='Kod_producenta']").text)
 
-    # print(ET.tostring(selected_products[3], encoding='unicode', method='xml'))
+    if mode == 'test':
 
-    # single_product = selected_products[3]
-    # print(ET.tostring(single_product, encoding='unicode', method='xml'))
+        with open('default_product_values.json') as def_file:
+            default_data = json.load(def_file)
 
-    selecto = selected_products[:2]
+        with open('manufacturers_dict.json') as man_file:
+            default_data['id_manufacturer'] = json.load(man_file)[brand]
 
-    with open('default_product_values.json') as def_file:
-        default_data = json.load(def_file)
+        for single_product in selected_products[:max_products]:
+            data = default_data
 
-    with open('manufacturers_dict.json') as man_file:
-        default_data['id_manufacturer'] = json.load(man_file)[brand]
+            data['id_category_default'] = 2
+            data['reference'] = single_product.find("attrs/a[@name='Kod_producenta']").text
+            data['ean13'] = single_product.find("attrs/a[@name='EAN']").text
+            data['price'] = single_product.get('price')
+            data['wholesale_price'] = str(round(float(data['price'])/price_ratio, 2))
+            data['name'] = single_product.find('name').text
+            data['link_rewrite'] = data['name'].lower().replace(' ', '-')
+            data['description'] = single_product.find('desc').text.split('div class')[0]
+            data['description_short'] = single_product.find('desc').text.split('</p><p>')[0]
 
-    for single_product in selecto:
-        data = default_data
+            data['meta_title'] = truncate_string(data['name'], 70)
+            data['meta_description'] = truncate_string(data['description'][3:].split('.')[0] + '.', 160)
 
-        data['id_category_default'] = 2
-        data['reference'] = single_product.find("attrs/a[@name='Kod_producenta']").text
-        data['ean13'] = single_product.find("attrs/a[@name='EAN']").text
-        data['price'] = single_product.get('price')
-        data['wholesale_price'] = str(round(float(data['price'])/price_ratio, 2))
-        data['name'] = single_product.find('name').text
-        data['link_rewrite'] = data['name'].lower().replace(' ', '-')
-        data['description'] = single_product.find('desc').text.split('div class')[0]
-        data['description_short'] = single_product.find('desc').text.split('</p><p>')[0]
+            for x in ['meta_description', 'meta_title', 'link_rewrite', 'name', 'description', 'description_short']:
+                data[x] = {'language': {'attrs': {'id': '2'}, 'value': data[x]}}
 
-        data['meta_title'] = truncate_string(data['name'], 70)
-        data['meta_description'] = truncate_string(data['description'][3:].split('.')[0] + '.', 160)
+            product_info = {'product': data}
+            print(product_info)
 
-        for x in ['meta_description', 'meta_title', 'link_rewrite', 'name', 'description', 'description_short']:
-            data[x] = {'language': {'attrs': {'id': '2'}, 'value': data[x]}}
-
-        product_info = {'product': data}
-        print(product_info)
-        # prestashop.add('products', product_info)
+            if mode == 'add':
+                prestashop.add('products', product_info)
 
     print('\nFunction completed')
 
 
-add_from_xml(file_name='luminosa_feed.xml', brand='Germaine de Capuccini')
-prestashop.delete('products', 778)
+add_from_xml(file_name='luminosa_feed.xml', brand='Germaine de Capuccini', mode='test', max_products=1)
+# prestashop.delete('products', 778)
