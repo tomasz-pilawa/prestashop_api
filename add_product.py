7814896273cca22c73a824b468f087248a9d2f0b
Tuan <tomasz.pilawa@gmail.com>
2023-06-26 11:48:49 +0200

    Move add_product_from_csv to obsolete_functions.py

--- add_product.py
+++ add_product.py
@@ -5,7 +5,6 @@ from prestapyt import PrestaShopWebServiceDict
 import xml.etree.ElementTree as ET
 import openai
 import requests
-import api_products as ap
 
 api_url = os.getenv('quelinda_link')
 api_key = os.getenv('quelinda_pass')
@@ -21,39 +20,6 @@ def truncate_string(text, max_length=70):
     else:
         truncated_text = text
     return truncated_text
-
-
-def add_product_from_csv(product):
-    with open(product) as file:
-        reader = csv.reader(file)
-        header = next(reader)
-        row = next(reader)
-        data = dict(zip(header, row))
-
-    with open('manufacturers_dict.json') as man_file:
-        id_manufacturer = json.load(man_file)[data['manufacturer_name']]
-
-    data['id_manufacturer'] = id_manufacturer
-    data['wholesale_price'] = str(round(int(data['price']) / 1.87, 2))
-    data['link_rewrite'] = data['name'].lower().replace(' ', '-')
-
-    with open('default_product_values.json') as def_file:
-        default_values = json.load(def_file)
-
-    data.update(default_values)
-
-    data.pop('manufacturer_name')
-
-    for x in ['meta_description', 'meta_title', 'link_rewrite', 'name', 'description', 'description_short']:
-        data[x] = {'language': {'attrs': {'id': '2'}, 'value': data[x]}}
-
-    print('Inserting the product...')
-
-    product_info = {'product': data}
-    print(product_info)
-    prestashop.add('products', product_info)
-
-    return data
 
 
 def add_from_xml(file_name, brand=None, mode='print', price_ratio=1.87, max_products=3, add_product=0,
--- obsolete_functions.py
+++ obsolete_functions.py
@@ -133,3 +133,36 @@ def get_products_2(brand=None, to_print=0):
     else:
         print("None brand given or the brand doesn't exist")
 
+
+def add_product_from_csv(product):
+    with open(product) as file:
+        reader = csv.reader(file)
+        header = next(reader)
+        row = next(reader)
+        data = dict(zip(header, row))
+
+    with open('manufacturers_dict.json') as man_file:
+        id_manufacturer = json.load(man_file)[data['manufacturer_name']]
+
+    data['id_manufacturer'] = id_manufacturer
+    data['wholesale_price'] = str(round(int(data['price']) / 1.87, 2))
+    data['link_rewrite'] = data['name'].lower().replace(' ', '-')
+
+    with open('default_product_values.json') as def_file:
+        default_values = json.load(def_file)
+
+    data.update(default_values)
+
+    data.pop('manufacturer_name')
+
+    for x in ['meta_description', 'meta_title', 'link_rewrite', 'name', 'description', 'description_short']:
+        data[x] = {'language': {'attrs': {'id': '2'}, 'value': data[x]}}
+
+    print('Inserting the product...')
+
+    product_info = {'product': data}
+    print(product_info)
+    prestashop.add('products', product_info)
+
+    return data
+
