9caf03ec0ac4117477719d5e72e9860c258ddc8b
Tuan <tomasz.pilawa@gmail.com>
2023-07-03 19:12:25 +0200

    Fix Stock emergency function

--- pricing.py
+++ pricing.py
@@ -37,4 +37,41 @@ def fix_prices(limit=2, file=None):
         prestashop.edit('products', product)
 
 
-fix_prices(limit=800, file='new_prices_urodama_07_2023.xml')
+# fix_prices(limit=800, file='new_prices_urodama_07_2023.xml')
+
+
+def fix_stock(file_old=None, file_new=None):
+
+    prestashop = PrestaShopWebServiceDict(api_url_urodama, api_key_urodama)
+
+    tree_old = ET.parse(f'data/{file_old}')
+    root_old = tree_old.getroot()
+    all_products_old = root_old.findall('o')
+    indexes_old = [int(p.get('id')) for p in all_products_old]
+
+    tree_new = ET.parse(f'data/{file_new}')
+    root_new = tree_new.getroot()
+    all_products_new = root_new.findall('o')
+    indexes_new = [int(p.get('id')) for p in all_products_new]
+
+    indexes_to_disable = [i for i in indexes_new if i not in indexes_old]
+    products_to_disable = [p for p in all_products_new if int(p.get('id')) in indexes_to_disable]
+
+    for p in products_to_disable:
+        p_id = int(p.get('id'))
+
+        product = prestashop.get('products', p_id)
+
+        product['product']['active'] = 0
+        product['product'].pop('manufacturer_name')
+        product['product'].pop('quantity')
+
+        # if int(product['product']['position_in_category']['value']) < 1:
+        #     product['product']['position_in_category']['value'] = str(1)
+
+        print(product['product'])
+
+        prestashop.edit('products', product)
+
+
+# fix_stock(file_old='new_prices_urodama_07_2023.xml', file_new='ceneo_urodama_after_update_too_many.xml')
